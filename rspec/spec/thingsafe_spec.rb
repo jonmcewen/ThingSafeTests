040dfaf1064455ec4f4188e9bb74226c9ed81c22
Jon McEwen <jon@Jonathans-MacBook-Pro.local>
2013-03-30 18:02:13 +0000

    PUT returns 200

--- rspec/spec/thingsafe_spec.rb
+++ rspec/spec/thingsafe_spec.rb
@@ -1,11 +1,16 @@
 require 'httparty'
+require 'json'
 
 url_base="http://localhost:8080/"
 
 describe "ThingSafe" do
   it "stores a thing" do
-    thing = { :title => "My Thing" }
-    response = HTTParty.post(url_base + "hello-world", thing)
+    thing = { :title => "My Thing" }.to_json
+    headers = { 'Content-Type' => 'application/json' }
+    response = HTTParty.put(url_base + "mythings", :body => thing, :headers => headers)
+
     response.code.should eq(200)
   end
 end
+
+
